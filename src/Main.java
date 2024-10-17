b0d26ce97eb061628c8dedcee56f0030135ea55b
stan10 <s97754@pollub.edu.pl>
2024-10-17 08:35:21 +0200

    feat: added new texts

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world2!");
+        System.out.println("Hello world3!");
+        System.out.println("asdadadadapkodad");
+        System.out.println("qqqqqqqqqqqqqqqq");
+        System.out.println("frrrrrrrrrrrrrr");
     }
 }
